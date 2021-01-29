523e448b72d3f66d0459f87d62d7aa9beda0cdd0
Zazsona <40435178+Zazsona@users.noreply.github.com>
2021-01-29 21:06:16 +0000

    Add bStats support.

--- src/main/java/com/Zazsona/decorheads/Core.java
+++ src/main/java/com/Zazsona/decorheads/Core.java
@@ -1,5 +1,6 @@
 package com.zazsona.decorheads;
 
+import org.bstats.bukkit.Metrics;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Core extends JavaPlugin
@@ -9,6 +10,8 @@ public class Core extends JavaPlugin
     {
         getConfig().options().copyDefaults(true);
         saveConfig();
+        int pluginId = 10174;
+        Metrics metrics = new Metrics(this, pluginId);
         getServer().getPluginManager().registerEvents(new HeadDropListener(), this);
         getServer().getPluginManager().registerEvents(new PlacedHeadRetriever(), this);
         this.getCommand("DecorHeads").setExecutor(new DecorHeadsCommand());
