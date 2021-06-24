48dfb0974a1e92ca5256c9313350f3a850d5289f
Zazsona <40435178+Zazsona@users.noreply.github.com>
2021-06-24 03:34:32 +0100

    Reshuffle permissions & define new commands structure.

---
+++ src/main/java/com/zazsona/decorheads/Permissions.java
@@ -0,0 +1,10 @@
+package com.zazsona.decorheads;
+
+public class Permissions
+{
+
+    public static final String PLUGIN_PREFIX = Core.PLUGIN_NAME.toLowerCase() + ".";
+    public static final String WIKI = PLUGIN_PREFIX + "WIKI";
+    public static final String SUMMON_HEAD = PLUGIN_PREFIX + "summonhead";
+    public static final String ADMIN = PLUGIN_PREFIX + "admin";
+}
