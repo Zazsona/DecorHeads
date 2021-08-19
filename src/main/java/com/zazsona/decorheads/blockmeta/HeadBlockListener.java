d1bce5b07819baf6fb28b5a4cb508a8a4ab656be
Zazsona <40435178+Zazsona@users.noreply.github.com>
2021-08-19 01:13:39 +0100

    Stop DecorHeads cancelling events for unrecognised heads.

--- src/main/java/com/zazsona/decorheads/blockmeta/HeadBlockListener.java
+++ src/main/java/com/zazsona/decorheads/blockmeta/HeadBlockListener.java
@@ -32,10 +32,12 @@ public class HeadBlockListener implements Listener
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onBlockBreak(BlockBreakEvent e)
     {
-        if (e.getPlayer().getGameMode() != GameMode.CREATIVE && e.getBlock().getType() == Material.PLAYER_HEAD)
+        Block block = e.getBlock();
+        if (e.getPlayer().getGameMode() != GameMode.CREATIVE && block.getType() == Material.PLAYER_HEAD)
         {
-            e.setDropItems(false);
-            dropHeadFromBlock(e.getBlock());
+            boolean headDropped = dropHeadFromBlock(block);
+            if (headDropped)
+                e.setDropItems(false);
         }
     }
 
@@ -46,10 +48,10 @@ public class HeadBlockListener implements Listener
         while (blockIterator.hasNext())
         {
             Block block = (Block) blockIterator.next();
-            if (BlockMetaLogger.getInstance().isMetadataSet(block.getLocation(), ID_KEY))
+            boolean headDropped = dropHeadFromBlock(block);
+            if (headDropped)
             {
-                blockIterator.remove();
-                dropHeadFromBlock(block);
+                blockIterator.remove();         // Effectively, manually "explode" the block, so we call the shots on the drops.
                 block.setType(Material.AIR);
             }
         }
@@ -62,10 +64,10 @@ public class HeadBlockListener implements Listener
         while (blockIterator.hasNext())
         {
             Block block = (Block) blockIterator.next();
-            if (BlockMetaLogger.getInstance().isMetadataSet(block.getLocation(), ID_KEY))
+            boolean headDropped = dropHeadFromBlock(block);
+            if (headDropped)
             {
-                blockIterator.remove();
-                dropHeadFromBlock(block);
+                blockIterator.remove();         // Effectively, manually "explode" the block, so we call the shots on the drops.
                 block.setType(Material.AIR);
             }
         }
@@ -76,9 +78,9 @@ public class HeadBlockListener implements Listener
     {
         for (Block movingBlock : e.getBlocks())
         {
-            if (movingBlock.getType() == Material.PLAYER_HEAD && BlockMetaLogger.getInstance().isMetadataSet(movingBlock.getLocation(), ID_KEY))
+            boolean headDropped = dropHeadFromBlock(movingBlock);
+            if (headDropped)
             {
-                dropHeadFromBlock(movingBlock);
                 movingBlock.setType(Material.AIR);
             }
         }
@@ -87,15 +89,12 @@ public class HeadBlockListener implements Listener
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onBlockBurn(BlockBurnEvent e)
     {
-        if (e.getBlock().getType() == Material.PLAYER_HEAD)
+        Block block = e.getBlock();
+        boolean headDropped = dropHeadFromBlock(block);
+        if (headDropped)
         {
-            Block block = e.getBlock();
-            if (BlockMetaLogger.getInstance().isMetadataSet(block.getLocation(), ID_KEY))
-            {
-                e.setCancelled(true);
-                dropHeadFromBlock(block);
-                block.setType(Material.AIR);
-            }
+            e.setCancelled(true);
+            block.setType(Material.AIR);
         }
     }
 
@@ -103,51 +102,71 @@ public class HeadBlockListener implements Listener
     public void onFromTo(BlockFromToEvent e)
     {
         Block block = e.getToBlock();
-        if (block.getType() == Material.PLAYER_HEAD)
+        boolean headDropped = dropHeadFromBlock(block);
+        if (headDropped)
         {
-            if (BlockMetaLogger.getInstance().isMetadataSet(block.getLocation(), ID_KEY))
-            {
-                e.setCancelled(true);
-                dropHeadFromBlock(block);
-                block.setType(Material.AIR);
-            }
+            e.setCancelled(true);
+            block.setType(Material.AIR);
         }
     }
 
     private boolean dropHeadFromBlock(Block block)
     {
-        boolean headDropped = false;
-        BlockMetaLogger metaLogger = BlockMetaLogger.getInstance();
-        Location location = block.getLocation();
-        if (metaLogger.isMetadataSet(location, ID_KEY))
+        if (isDecorHeadsHead(block))
         {
+            BlockMetaLogger metaLogger = BlockMetaLogger.getInstance();
+            Location location = block.getLocation();
             String headKey = metaLogger.getMetadata(location, ID_KEY);
             IHead head = HeadLoader.getInstance().getLoadedHeads().get(headKey);
-            if (head != null)
+            ItemStack item;
+            if (head instanceof PlayerHead)
             {
-                ItemStack item;
-                if (head instanceof PlayerHead)
+                PlayerHead playerHead = (PlayerHead) head;
+                String uuid = metaLogger.getMetadata(location, PLAYER_ID_KEY);
+                if (metaLogger.isMetadataSet(location, TEXTURE_KEY))
                 {
-                    PlayerHead playerHead = (PlayerHead) head;
-                    String uuid = metaLogger.getMetadata(location, PLAYER_ID_KEY);
-                    if (metaLogger.isMetadataSet(location, TEXTURE_KEY))
-                    {
-                        String texture = metaLogger.getMetadata(location, TEXTURE_KEY);
-                        item = playerHead.createItem(UUID.fromString(uuid), texture);
-                    }
-                    else
-                        item = playerHead.createItem(UUID.fromString(uuid));
+                    String texture = metaLogger.getMetadata(location, TEXTURE_KEY);
+                    item = playerHead.createItem(UUID.fromString(uuid), texture);
                 }
                 else
-                    item = head.createItem();
-                block.getWorld().dropItemNaturally(block.getLocation(), item);
-                headDropped = true;
+                    item = playerHead.createItem(UUID.fromString(uuid));
             }
-            metaLogger.removeMetadata(location, ID_KEY);
-            metaLogger.removeMetadata(location, PLAYER_ID_KEY);
-            metaLogger.removeMetadata(location, TEXTURE_KEY);
+            else
+                item = head.createItem();
+            block.getWorld().dropItemNaturally(block.getLocation(), item);
+            clearHeadMeta(location);
+            return true;
         }
-        return headDropped;
+        else
+        {
+            Location location = block.getLocation();
+            clearHeadMeta(location);
+            return false;
+        }
+    }
+
+    private boolean isDecorHeadsHead(Block block)
+    {
+        if (block.getType() == Material.PLAYER_HEAD)
+        {
+            BlockMetaLogger metaLogger = BlockMetaLogger.getInstance();
+            Location location = block.getLocation();
+            if (metaLogger.isMetadataSet(location, ID_KEY))
+            {
+                String headKey = metaLogger.getMetadata(location, ID_KEY);
+                IHead head = HeadLoader.getInstance().getLoadedHeads().get(headKey);
+                return head != null;
+            }
+        }
+        return false;
+    }
+
+    private void clearHeadMeta(Location location)
+    {
+        BlockMetaLogger metaLogger = BlockMetaLogger.getInstance();
+        metaLogger.removeMetadata(location, ID_KEY);
+        metaLogger.removeMetadata(location, PLAYER_ID_KEY);
+        metaLogger.removeMetadata(location, TEXTURE_KEY);
     }
 
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
