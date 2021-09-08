68ff0e4198cb7a5d858d83b038e4fa3b0f08fcb2
Zazsona <40435178+Zazsona@users.noreply.github.com>
2021-09-08 19:47:45 +0100

    Have craft drops be dependent on the crafts complete, rather than each craft event.

--- src/main/java/com/zazsona/decorheads/headsources/CraftDropHeadSource.java
+++ src/main/java/com/zazsona/decorheads/headsources/CraftDropHeadSource.java
@@ -3,6 +3,7 @@ package com.zazsona.decorheads.headsources;
 import com.zazsona.decorheads.config.PluginConfig;
 import com.zazsona.decorheads.headdata.IHead;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -10,6 +11,8 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.inventory.CraftItemEvent;
 import org.bukkit.inventory.ItemStack;
+
+import java.util.HashMap;
 
 public class CraftDropHeadSource extends DropHeadSource
 {
@@ -24,14 +27,37 @@ public class CraftDropHeadSource extends DropHeadSource
     }
 
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-    public ItemStack onItemCrafted(CraftItemEvent e)    //TODO: Consider multiple rolls if multiple items are crafted
+    public ItemStack onItemCrafted(CraftItemEvent e)
     {
         if (PluginConfig.isDropSourceEnabled(getSourceType()) && passFilters(e))
         {
             Player player = (Player) e.getWhoClicked();
             World world = e.getWhoClicked().getWorld();
             Location location = e.getInventory().getLocation();
-            return super.dropHead(world, location, getBaseDropRate(), player, null);
+
+            int totalCrafted = 0;
+            if (e.isShiftClick())
+            {
+                ItemStack[] matrixItems = e.getInventory().getMatrix();
+                int lowestQuantity = Integer.MAX_VALUE;
+                for (ItemStack itemStack : matrixItems)
+                {
+                    if (itemStack != null && itemStack.getType() == Material.AIR && itemStack.getAmount() < lowestQuantity)
+                        lowestQuantity = itemStack.getAmount();
+                }
+                totalCrafted = lowestQuantity * e.getRecipe().getResult().getAmount();
+            }
+            else
+                totalCrafted = e.getRecipe().getResult().getAmount();
+
+            int totalHeadDrops = 0;
+            for (int i = 0; i < totalCrafted; i++)
+            {
+                if (super.rollDrop(getBaseDropRate()))
+                    totalHeadDrops++;
+            }
+
+            return super.dropHead(world, location, player, null, totalHeadDrops);
         }
         return null;
     }
