c7add28b1b57a2e4b162debf479b201068f8d767
Zazsona <40435178+Zazsona@users.noreply.github.com>
2022-12-19 18:31:14 +0000

    Add getEmptyConfigData() and validateConfigData() to YamlConfigWrapper

--- src/main/java/com/zazsona/decorheads/config/YamlConfigWrapper.java
+++ src/main/java/com/zazsona/decorheads/config/YamlConfigWrapper.java
@@ -11,16 +11,31 @@ public abstract class YamlConfigWrapper implements IYamlConfigWrapper
     protected FileConfiguration config;
     protected File saveLocation;
 
+    /**
+     * Creates a new wrapper around the provided config. If the provided config is null, an empty config will be generated
+     * @param config the config to wrap
+     * @param saveLocation the default save location
+     * @throws IllegalArgumentException config is invalid for this wrapper
+     */
     public YamlConfigWrapper(FileConfiguration config, File saveLocation)
     {
+        if (config == null)
+            config = getEmptyConfigData();
+        if (!validateConfigData(config))
+            throw new IllegalArgumentException("The provided FileConfiguration is invalid for this config type.");
+
         this.config = config;
         this.saveLocation = saveLocation;
     }
 
+    /**
+     * Creates a new wrapper around the config in the file. If the file is null or does not exist, an empty config will be generated
+     * @param configFile the file containing config data
+     * @throws IllegalArgumentException config is invalid for this wrapper
+     */
     public YamlConfigWrapper(File configFile)
     {
-        FileConfiguration configContents = YamlConfiguration.loadConfiguration(configFile);
-
+        FileConfiguration configContents = loadConfigFromFile(configFile);
         this.config = configContents;
         this.saveLocation = configFile;
     }
@@ -62,7 +77,46 @@ public abstract class YamlConfigWrapper implements IYamlConfigWrapper
 
     public void reloadConfig()
     {
-        FileConfiguration configContents = YamlConfiguration.loadConfiguration(saveLocation);
+        FileConfiguration configContents = loadConfigFromFile(saveLocation);
         config = configContents;
+    }
+
+    /**
+     * Gets an empty config supported by this wrapper
+     * @return an empty, but valid, config
+     */
+    protected FileConfiguration getEmptyConfigData()
+    {
+        return new YamlConfiguration();
+    }
+
+    /**
+     * Validates the config is compatible with this wrapper
+     * @param configData the underlying config
+     * @return boolean on validity
+     */
+    protected boolean validateConfigData(FileConfiguration configData)
+    {
+        return (configData != null);
+    }
+
+    /**
+     * Attempts to load configuration data from the provided file.
+     * If the file is null or does not exist, an empty config will be returned instead.
+     * @param configFile the file to load from.
+     * @return a configuration
+     */
+    protected FileConfiguration loadConfigFromFile(File configFile)
+    {
+        FileConfiguration configContents;
+        if (configFile != null && configFile.exists())
+            configContents = YamlConfiguration.loadConfiguration(configFile);
+        else
+            configContents = getEmptyConfigData();
+
+        if (!validateConfigData(config))
+            throw new IllegalArgumentException("The provided FileConfiguration is invalid for this config type.");
+
+        return configContents;
     }
 }
